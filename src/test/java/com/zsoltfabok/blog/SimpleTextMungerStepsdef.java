e0cc1d0b3dd6a1243e42247e9b1eff1679a81a77
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2013-11-21 20:10:02 +0100

    refactor: stubbing in given

--- src/test/java/com/zsoltfabok/blog/SimpleTextMungerStepsdef.java
+++ src/test/java/com/zsoltfabok/blog/SimpleTextMungerStepsdef.java
@@ -13,14 +13,12 @@ public class SimpleTextMungerStepsdef {
     private Munger munger;
     private SentenceHelper sentenceHelper;
     private String result;
-    private String inputWord;
-    private String mungedWord;
 
     @Given("^I have a mocked munger which always returns \"([^\"]*)\" for \"([^\"]*)\"$")
     public void I_have_a_mocked_munger_which_always_returns_(String munged, String word) {
         munger = mock(Munger.class);
-        inputWord = word;
-        mungedWord = munged;
+        when(munger.munge(word)).thenReturn(munged);
+
     }
 
     @Given("^I have an instance of my class$")
@@ -32,7 +30,6 @@ public class SimpleTextMungerStepsdef {
 
     @When("^I call my method with \"([^\"]*)\"$")
     public void I_call_my_method_with_(String word) {
-        when(munger.munge(inputWord)).thenReturn(mungedWord);
         result = simpleTextMunger.execute(word);
     }
 
