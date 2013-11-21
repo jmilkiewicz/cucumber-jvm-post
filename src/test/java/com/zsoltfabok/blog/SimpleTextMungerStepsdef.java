b89c083f616da214bf210613b5484fccb751c640
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2013-11-21 20:07:43 +0100

    changing imports to reflect new cucumber

--- src/test/java/com/zsoltfabok/blog/SimpleTextMungerStepsdef.java
+++ src/test/java/com/zsoltfabok/blog/SimpleTextMungerStepsdef.java
@@ -3,9 +3,9 @@ package com.zsoltfabok.blog;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import cucumber.annotation.en.Given;
-import cucumber.annotation.en.Then;
-import cucumber.annotation.en.When;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class SimpleTextMungerStepsdef {
 
