aa84708a065f1233c291dbe902e5f415c49667f0
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2013-11-22 10:58:47 +0100

    new imports reflecting new cucumber

--- src/test/java/com/zsoltfabok/blog/WebTextMungerStepsdef.java
+++ src/test/java/com/zsoltfabok/blog/WebTextMungerStepsdef.java
@@ -2,13 +2,14 @@ package com.zsoltfabok.blog;
 
 import static org.junit.Assert.assertEquals;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-import cucumber.annotation.en.Given;
-import cucumber.annotation.en.Then;
-import cucumber.annotation.en.When;
+
 
 public class WebTextMungerStepsdef {
 
